d58debc7b43f83c39b5f87bfacfcdf67c71fe24a
fillahaufi <fillahaufi140101@gmail.com>
2024-10-06 01:00:13 +0700

    feat: added tailwind and primevue

--- circle-fe/src/main.ts
+++ circle-fe/src/main.ts
@@ -3,6 +3,8 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import PrimeVue from 'primevue/config'
+
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +12,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(PrimeVue, {
+  unstyled: true
+});
+
 
 app.mount('#app')
